996008b67acdead2ce426973954b959d16fec3d3
AshleySi <junke.si@grab.com>
2019-10-30 12:48:06 +0800

    use async

--- src/service.ts
+++ src/service.ts
@@ -1,15 +1,30 @@
 import axios from 'axios';
+import * as model from './model';
 
 const baseUrl = 'http://localhost:5000';
-const customerListUrl = `{baseUrl}/customers`;
-const goalUrl = (id: String) => `{baseUrl}/goal_{id}`;
+const customerListUrl = `${baseUrl}/customers`;
+const goalUrl = (id: String) => `${baseUrl}/goal_${id}`;
 
 export default class APIService {
-  public static getCustomerList() {
-    return axios.get(customerListUrl);
+  public static async getCustomerList(): Promise<model.Customer[]> {
+    try {
+      const customers = (await axios.get(customerListUrl)).data;
+      console.log(customers);
+      return customers;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 
-  public static getCustomerGoal(customerId: String) {
-    return axios.get(goalUrl(customerId));
+  public static async getCustomerGoal(customerId: String): Promise<model.Goal|undefined> {
+    try {
+      const goal = (await axios.get(goalUrl(customerId))).data;
+      console.log(goal);
+      return goal;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 }
