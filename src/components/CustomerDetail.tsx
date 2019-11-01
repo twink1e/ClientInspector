720f4921250d0f0d4dab926d1c68f3b480826aa8
AshleySi <junke.si@grab.com>
2019-11-01 15:10:00 +0800

    add loading

--- src/components/CustomerDetail.tsx
+++ src/components/CustomerDetail.tsx
@@ -7,23 +7,29 @@ type Props = {
 }
 
 type State = {
-  goal: Goal | undefined
+  goal: Goal | undefined,
+  loading: boolean
 }
 
 export default class CustomerDetail extends React.Component<Props, State> {
   state: State = {
-    goal: undefined
+    goal: undefined,
+    loading: true
   };
 
   componentDidMount() {
     APIService.getCustomerGoal(this.props.id)
     .then((res: Goal | undefined) => this.setState(state => ({
-      goal: res
+      goal: res,
+      loading: false
     })
     ));
   }
 
   render() {
+    if (this.state.loading) {
+      return <div/>;
+    }
     const goal = this.state.goal;
     if (goal == undefined) {
       return <h1>Invalid customer, or this customer has no goal.</h1>;
